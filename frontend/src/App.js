ebe0a9819fdddcf30b5738462c6cc06036fe814b
SakthiV4 <sakthiprasath809804@gmail.com>
2025-09-01 00:18:30 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
